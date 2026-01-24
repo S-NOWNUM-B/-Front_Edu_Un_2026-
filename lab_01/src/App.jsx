38b6822add9387b8768e80b323a425bdb8387734
SNOWNUMB <mamayev.stas@gmail.com>
2026-01-24 10:55:22 +0500

    Выполнено Task 1 / Lab_01

---
+++ lab_01/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import FragmentLayout from './FragmentLayout.jsx';
+
+function App() {
+    return (
+        <FragmentLayout />
+    )
+}
+
+export default App;
---
+++ lab_01/src/FragmentLayout.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function FragmentLayout() {
+    return (
+        <>
+            <header>
+                <h1>Frontend Education</h1>
+            </header>
+
+            <main>
+                <p>Это простой пример компонента с FragmentLayout</p>
+            </main>
+
+            <footer>
+                <p>&copy; 2026 Все права защищены</p>
+            </footer>
+        </>
+    )
+}
+
+export default FragmentLayout
