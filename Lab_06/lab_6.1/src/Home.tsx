bb1d52df8cb8090ff17c5f539c777966a7c25ebe
SNOWNUMB <mamayev.stas@gmail.com>
2026-02-21 10:20:32 +0500

    feat: добавлен компонент Home с приветственным сообщением для портала студентов

---
+++ Lab_06/lab_6.1/src/Home.tsx
@@ -0,0 +1,8 @@
+const Home = () => {
+	<div>
+		<h1>Welcome to the Student Portal</h1>
+		<p>Here you can find information about your courses, grades, and more.</p>
+	</div>
+};
+
+export default Home;
