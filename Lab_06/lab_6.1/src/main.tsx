dff15e427a5490361b00f1ee5f38d6260aeb9a46
SNOWNUMB <mamayev.stas@gmail.com>
2026-02-21 10:19:18 +0500

    feat: добавлен роутинг с использованием react-router-dom и компонент Layout

--- Lab_06/lab_6.1/src/main.tsx
+++ Lab_06/lab_6.1/src/main.tsx
@@ -1,9 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Layout from './Layout';
+import Home from './Home';
+import Courses from './Courses';
+import About from './About';
+import NotFound from './NotFound';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: 'courses', element: <Courses /> },
+      { path: 'about', element: <About /> },
+      { path: '*', element: <NotFound /> }
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+);
