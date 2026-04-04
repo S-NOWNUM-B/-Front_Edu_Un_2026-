bfb74c5c5452903392dfaba0f5d46061434522a8
SNOWNUMB <mamayev.stas@gmail.com>
2026-04-04 23:44:27 +0500

    feat: добавлен компонент TodoList с функциональностью добавления, удаления и переключения состояния задач feat: добавлены тесты для компонента TodoList feat: добавлены стили для приложения feat: настроен Vite и TypeScript для проекта feat: добавлены иконки React и Vite feat: добавлен главный файл приложения и конфигурация для Vercel

---
+++ Lab_10/lab_10.2/vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react-dom') || id.includes('react/jsx-dev-runtime')) {
+              return 'vendor';
+            }
+          }
+        },
+      },
+    },
+  },
+  server: {
+    port: 3000,
+  },
+});
